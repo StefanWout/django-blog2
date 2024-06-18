3636bb9768674d9ede9a2362d65c354d9d089a45
Stefan Woutersen <stefanwoutersen@gmail.com>
2024-06-18 12:07:43 +0000

    made some kind of migration

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-qw6xezw6msr04gswyv&+1mn__&k#)cpeenx+9m)_ko6sc@!s!o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-stefanwout-djangoblog2-vq31slr76to.ws.codeinstitute-ide.net', '.herokuapp.com', 'https://8000-stefanwout-djangoblog2-vq31slr76to.ws.codeinstitute-ide.net', 'https://8000-stefanwout-djangoblog2-vq31slr76to.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-stefanwout-djangoblog2-vq31slr76to.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
