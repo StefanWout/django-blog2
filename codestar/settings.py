2475e95f3ef3ebf5efbdd864b9d50fc22c60b7c0
Stefan Woutersen <stefanwoutersen@gmail.com>
2024-06-19 15:21:42 +0000

    added summernote

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-qw6xezw6msr04gswyv&+1mn__&k#)cpeenx+9m)_ko6sc@!s!o
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-stefanwout-djangoblog2-vq31slr76to.ws.codeinstitute-ide.net', '.herokuapp.com', 'https://8000-stefanwout-djangoblog2-vq31slr76to.ws.codeinstitute-ide.net']
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
